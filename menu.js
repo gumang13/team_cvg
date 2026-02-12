458ad972e44ef28e1a75d11e1a82e5dbbe79b6d7
osy01236 <cksdyd54321@gmail.com>
2026-02-12 23:02:10 +0900

    진짜진짜최종

--- menu.js
+++ menu.js
@@ -1,5 +1,6 @@
 const menu = document.querySelectorAll('.메뉴');
 
+
 menu.forEach(a => {
    
     a.addEventListener('mouseenter', () => {
@@ -12,3 +13,23 @@ menu.forEach(a => {
         a.style.transform = "scale(1.0)";
     });
 });
+
+
+function requestCheck() {
+    let lostid = document.getElementById("lostitemid");
+    let claimid = document.getElementById("claimid");
+    
+
+    if(lostid.value == '' || claimid.value == ''){
+    alert("모든 ID를 입력해주세요.");
+    return;
+}
+    alert("반환이 완료되었습니다.");
+    location.reload(); // 새로고침
+
+}
+
+function clearInput(){
+  document.getElementById("lostitemid").value = "";
+  document.getElementById("claimid").value = "";
+}
